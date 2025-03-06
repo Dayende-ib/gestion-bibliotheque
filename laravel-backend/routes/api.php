72ea203b09cbc5a26c67382d2db97b95e6afeb95
Jojo-onedev <bationojonathan5@gmail.com>
2025-03-06 21:23:29 +0000

    Ajout de la route de test d' api

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -6,20 +6,20 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\LoanController;
 
-
 // Ajout de la route pour la création de compte et la connexion
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Book Management Routes
     Route::get('/books', [BookController::class, 'index']);
     Route::post('/books', [BookController::class, 'store']);
     Route::get('/books/{book}', [BookController::class, 'show']);
     Route::put('/books/{book}', [BookController::class, 'update']);
     Route::delete('/books/{book}', [BookController::class, 'destroy']);
 
+    // Loan Management Routes
     Route::post('/loans', [LoanController::class, 'store']);
     Route::patch('/loans/{loan}', [LoanController::class, 'update']);
     Route::delete('/loans/{loan}', [LoanController::class, 'destroy']);
